f692dd6bb04d160fe62a4c67b3b9ebe4262426d7
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 21:40:28 -0700

    Fix routing

--- application/app.js
+++ application/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const router = express.Router()
 const port = 3000
 
-app.use(express.static('application'))
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
