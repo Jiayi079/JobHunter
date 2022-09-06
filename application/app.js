35897e2df10f3bc08e2c852ecb219d7f36a1281d
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 20:32:27 -0700

    Change port listener

--- application/app.js
+++ application/app.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-})
+app.use(express.static('application'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
