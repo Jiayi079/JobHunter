223d1237f3c61deca54e9a6ed47b81aa1d2cbf6d
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 20:11:26 -0700

    Serve index.html

--- application/app.js
+++ application/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
