6d6063f7e996b2614d4f6bb7043eb7016ebe795f
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 22:31:31 -0700

    Fix routing

--- application/app.js
+++ application/app.js
@@ -8,7 +8,7 @@ app.use(express.static('application'))
 app.use('css', express.static(__dirname, + 'application/css'))
 app.use('css', express.static(__dirname, + 'application/memberPages'))
 
-router.get('', function(req, res) {
+router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 })
 
