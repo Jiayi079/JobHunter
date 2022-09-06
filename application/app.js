f8403e957c2194dd2392e8bbbc11de72b88229fc
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 20:07:33 -0700

    Add port listener

--- application/app.js
+++ application/app.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('HEY!')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000, () => console.log('Server running on port 3000'))
