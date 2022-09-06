14fdb4c80bf700156aca90d621ff82100691c47d
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 20:13:25 -0700

    Fix typo

--- application/app.js
+++ application/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const port = 3000
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 })
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
