612c7b51f00ae4cc423ff54fd53d2c4a15560fe7
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-05 20:20:57 -0700

    Fix server bug

--- application/app.js
+++ application/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const port = 3000
+
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
