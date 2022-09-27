f944611e5224ecffdd38c5cbb940474921244036
Jiayi079 <jgu3@mail.sfsu.edu>
2022-09-26 22:06:17 -0700

    create basic view for login and register page

---
+++ application/main.js
@@ -0,0 +1,14 @@
+// dicynebt,addEventListener("DOMcontentLoaded", () => {
+//     const loginForm = document.getSelector("#login");
+//     const createAccountForm = document.getSelector("#creatAccount");
+
+//     document.getSelector("#linkCreateAccount").addEventListener("click", () => {
+//         loginForm.classList.add("form-hidden");
+//         createAccountForm.classList.remove("form-hidden");
+//     });
+
+//     document.getSelector("#linkLogin").addEventListener("click", () => {
+//         loginForm.classList.remove("form-hidden");
+//         createAccountForm.classList.add("form-hidden");
+//     });
+// });
