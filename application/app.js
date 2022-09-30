1751589179a7887a457ac3978cc5b54c4b68b761
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-30 16:31:11 -0700

    fix bug

--- application/app.js
+++ application/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
-const things = require('./things.js')
+const things = require('./things')
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
