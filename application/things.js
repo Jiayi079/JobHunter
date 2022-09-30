7ce502f2770f10b7aa7ab317d06b7bd1ff6db4b9
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-30 16:43:32 -0700

    setup testing

--- application/things.js
+++ application/things.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require('path')
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/test.html'));
   });
 
 module.exports = router;
