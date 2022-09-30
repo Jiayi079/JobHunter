bc931ad38659f533ecf0bcd605e148a129ee5683
ybsaatci <yagiz.saatchi@gmail.com>
2022-09-30 16:28:10 -0700

    refator routes

--- application/app.js
+++ application/app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
-
+const things = require('./things.js')
 const mysql = require('mysql')
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -21,7 +22,7 @@ app.use('css', express.static(__dirname, + 'application/css'))
 app.use('css', express.static(__dirname, + 'application/memberPages'))
 
 
-
+app.use('/things', things)
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
