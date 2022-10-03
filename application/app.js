2d2a49c4461052611d9fa58e6ca72f7a6e8a1048
ybsaatci <yagiz.saatchi@gmail.com>
2022-10-02 19:02:16 -0700

    add ejs templating

--- application/app.js
+++ application/app.js
@@ -2,37 +2,49 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
-const things = require('./things')
 const mysql = require('mysql')
 const { nextTick } = require('process')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Root@123',
-  database: 'sys'
-})
+const postListng = [
+  {
+    company: 'Google',
+    jobName: 'SWE'
+  },
+  {
+    company: 'Uber',
+    jobName: 'AI'
+  }
+]
 
-
-
-connection.connect()
-
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
 
 app.use(express.static('application'))
 app.use('css', express.static(__dirname, + 'application/css'))
 app.use('css', express.static(__dirname, + 'application/memberPages'))
 
+app.use(express.json())
+app.use(express.urlencoded())
 
-//Middleware function to log request protocol
-app.use('/things', function(req, res, next){
-  console.log("A request for things received at " + Date.now());
-  next();
-});
+function search(req, res, next) {
+  var searchTerm = req.query.search;
 
+  var category = req.query.catagory;
+}
 // Route handler that sends the response
-app.get('/things', function(req, res){
-  res.send('Things');
+app.get('/', function(req, res){
+  res.render('index')
 });
+
+app.get('/jobs', (req, res) => {
+  res.send(postListng)
+})
+
+app.post('/jobs', (req, res) => {
+  console.log(req.body)
+  postListng.push(req.body)
+  res.send(201)
+})
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
