5c4eacf50856fdc4a6cbf53354a1e1cab50ed868
Jiayi079 <jgu3@mail.sfsu.edu>
2022-10-09 20:53:25 -0700

    update for main page

--- application/main.js
+++ application/main.js
@@ -12,3 +12,12 @@
 //         createAccountForm.classList.add("form-hidden");
 //     });
 // });
+
+
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+  }
+  
+  const clearBtn = document.getElementById("clear-btn");
+  clearBtn.addEventListener("click", clearInput);
