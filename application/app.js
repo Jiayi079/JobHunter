babf8fb651c22c7b87dbb42031781d3baf2562fb
ybsaatci <yagiz.saatchi@gmail.com>
2022-10-25 02:00:57 -0700

    fix query

--- application/app.js
+++ application/app.js
@@ -111,7 +111,6 @@ passport.use(new passportLocal.Strategy({
         console.log(err)
       }
       userFound = result[0]
-      console.log(result[0])
       console.log(userFound.email)
       console.log(userFound.password)
     })
