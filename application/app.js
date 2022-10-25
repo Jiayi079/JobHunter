38c4492a45e91fdc21dccc30536ea42476a9ddf1
ybsaatci <yagiz.saatchi@gmail.com>
2022-10-25 02:32:01 -0700

    add testing

--- application/app.js
+++ application/app.js
@@ -106,7 +106,9 @@ passport.use(new passportLocal.Strategy({
   try {
     const query = "SELECT * From userData WHERE email = ?"
     const userFound = await database.query(query, [email])
-
+    console.log(userFound)
+    console.log(userFound[0].password)
+    console.log(userFound[0])
     if ( userFound[0] && password == userFound[0].password) {
       console.log("Success")
       done(null,userFound)
