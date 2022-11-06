e85de5468bc1349303a2c8e143d18d116ecd0eab
ybsaatci <yagiz.saatchi@gmail.com>
2022-11-06 00:47:38 -0700

    add callback to logout

--- application/app.js
+++ application/app.js
@@ -291,10 +291,12 @@ app.get('/about', (req, res) => {
   res.render('pages/about');
 })
 
-app.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
+app.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
